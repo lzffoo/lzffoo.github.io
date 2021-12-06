19520bd4cb380e9a7b1dcab42b557eed4a10f69a
lzfgit <1807409742@qq.com>
2021-12-06 10:59:22 +0800

    init

--- src/nes/index.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-//   Controller: require("./controller"),
-//   NES: require("./nes"),
-// };
-import {Controller} from './controller.js'
-import {NES} from './nes.js'
-let jsnes = {Controller,NES};
-export {jsnes}
